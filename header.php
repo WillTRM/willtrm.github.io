4128ea61b2958e2b29cbec378188f178abc04b1f
WillTRM <willtrm111@gmail.com>
2025-05-18 03:01:48 -0500

    added tetopear

---
+++ header.php
@@ -0,0 +1,13 @@
+<div class="container">
+            <a href="index.html">
+                <button type="button" class="nav-button">Home</button>
+            </a>
+            
+            <a href="portfolio.html">
+                <button type="button" class="nav-button">Portfolio</button>
+            </a>
+
+            <a href = "tetopear.html">
+                <button type="button" class="nav-button">Tetopear</button>
+            </a>
+        </div> 
---
+++ tetopear.js
@@ -0,0 +1,5 @@
+document.getElementById("tetopear").addEventListener("click", function(pearto_NOW)
+{
+    var audio = new Audio("tetopear.mp3");
+    audio.play();
+});
